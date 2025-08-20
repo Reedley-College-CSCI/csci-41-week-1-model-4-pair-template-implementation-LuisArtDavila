8bdc541616eb4746ac1f9f60487c368a8aeed4e9
Luis Davila <davila.luis.us@gmail.com>
2025-08-20 11:36:12 -0700

    Added test cases in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-// Your Names
+// Luis Davila, Xander Perez
 #include "Pair.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Add test cases here
+    Pair intPair(1, 2);
+    cout << intPair.getFirst() << endl; 
+    cout << intPair.getSecond() << endl;
+
+    Pair stringPair(string("apple"), string("banana"));
+    cout << stringPair.getFirst() << endl; 
+    cout << stringPair.getSecond() << endl;
+
+    Pair doublePair(1.25, 2.50);
+    cout << doublePair.getFirst() << endl; 
+    cout << doublePair.getSecond() << endl;
+
     return 0;
 }
